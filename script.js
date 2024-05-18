60a260e4d9eef50d954bc83565168bbb96b4b75e
Falgun Sarode <147540719+falgunsarode23@users.noreply.github.com>
2024-05-18 09:54:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,29 @@
 function fibonacci(num) {
-// your code here
+// your code
+	function fibonacci(num) {
+    if (num === 0) {
+        return 0;
+    } else if (num === 1) {
+        return 1;
+    }
+
+    let a = 0, b = 1;
+    for (let i = 2; i <= num; i++) {
+        let temp = a + b;
+        a = b;
+        b = temp;
+    }
+
+    return b;
+}
+
+// Test the function
+console.log(fibonacci(0));  // Output: 0
+console.log(fibonacci(1));  // Output: 1
+console.log(fibonacci(5));  // Output: 5
+console.log(fibonacci(10)); // Output: 55
+console.log(fibonacci(20)); // Output: 6765
+
 }
 
 module.exports = fibonacci;
